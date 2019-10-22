68224d570910c462798361f65fd20e5d6afd3fde
bgarcia95 <bgarcia.95@outlook.com>
2019-10-22 01:25:40 -0600

    Adding actions to read persons records

--- src/reducers/personasReducer.js
+++ src/reducers/personasReducer.js
@@ -1,7 +1,10 @@
 import {
   AGREGAR_PERSONA,
   AGREGAR_PERSONA_EXITO,
-  AGREGAR_PERSONA_ERROR
+  AGREGAR_PERSONA_ERROR,
+  COMENZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITOSA,
+  DESCARGA_PRODUCTOS_ERROR
 } from "../types";
 
 // creando su propio state
@@ -28,6 +31,25 @@ export default function(state = initialState, action) {
       return {
         ...state,
         error: action.payload
+      };
+    case COMENZAR_DESCARGA_PRODUCTOS:
+      return {
+        ...state,
+        loading: true
+      };
+    case DESCARGA_PRODUCTOS_EXITOSA:
+      return {
+        ...state,
+        productos: action.payload,
+        loading: false,
+        error: false
+      };
+    case DESCARGA_PRODUCTOS_ERROR:
+      return {
+        ...state,
+        productos: [],
+        error: true,
+        loading: false
       };
     default:
       return state;
